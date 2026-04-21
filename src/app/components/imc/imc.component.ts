a23ce720eb91182175aabb0294300789fb96901f
Val <valexx55@gmail.com>
2026-04-21 10:53:18 +0200

    propuesta imc

--- src/app/components/imc/imc.component.ts
+++ src/app/components/imc/imc.component.ts
@@ -1,5 +1,27 @@
 import { Component } from '@angular/core';
 
+
+
+/*
+
+   * Haced una APP que calcule el Índice de Masa Corporal de una persona
+   * la fórmula es la siguiente:
+   * 
+   *  IMC = peso (kg) / altura * altura(m)
+   * 
+   * si el imc < 16 - DESNUTRIDO
+   * si el imc >=16 y < 18 - DELGADO
+   * si el imc >=18 Y < 25 - IDEAL
+   * si el imc >=25 y < 31 - SOBREPESO
+   * si el imc >=31 - OBESO
+   * 
+   * hay que informar al usuario de su valor numérico
+   * y de su valor nominal (categoría)
+   * 
+   * //opcional: buscad una foto/imagen/dibujo reprsentativo de cada
+   * estado/resultado (delgado, etc) y ubicarla en la carpeta public
+   * 
+  */
 @Component({
   selector: 'app-imc',
   imports: [],
