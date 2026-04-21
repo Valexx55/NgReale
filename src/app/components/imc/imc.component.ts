deaa5d7a7970f64be384ad1b955dc52b2e34640f
Val <valexx55@gmail.com>
2026-04-21 11:56:59 +0200

    imc inicial

--- src/app/components/imc/imc.component.ts
+++ src/app/components/imc/imc.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -24,10 +25,56 @@ import { Component } from '@angular/core';
   */
 @Component({
   selector: 'app-imc',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './imc.component.html',
   styleUrl: './imc.component.css'
 })
 export class ImcComponent {
 
+
+    titulo: string = 'CALCULO DEL IMC';
+    peso:number=0;
+    altura:number=0;
+    //valores derivados
+    numerico:number=0;
+    lectura:string='';
+    foto:string='';
+
+    
+  static readonly FOTO_DESNUTRIDO: string = '/desnutrido.jpg';
+  static readonly FOTO_DELGADO: string = '/delgado.jpg';
+  static readonly FOTO_IDEAL: string = '/ideal.jpg';
+  static readonly FOTO_SOBREPESO: string = '/sobrepeso.jpg';
+  static readonly FOTO_OBESO: string = '/obeso.jpg';
+
+
+
+  calcularIMC() {
+    console.log('calcular imc boton tocado');
+    this.numerico = this.peso / (this.altura * this.altura);
+    //casting de String a numero +
+    this.numerico = +this.numerico.toFixed(2);
+    //this.numerico = parseFloat(this.numerico.toFixed(2));
+    if (this.numerico < 16) {
+      //desnutrido
+      this.lectura = 'DESNUTRIDO';
+      this.foto = ImcComponent.FOTO_DESNUTRIDO;
+    } else if (this.numerico >= 16 && this.numerico < 18) {
+      //delgado
+      this.lectura = 'DELGADO';
+      this.foto = ImcComponent.FOTO_DELGADO;
+    } else if (this.numerico >= 18 && this.numerico < 25) {
+      //ideal
+      this.lectura = 'IDEAL';;
+      this.foto = ImcComponent.FOTO_IDEAL;
+    } else if (this.numerico >= 25 && this.numerico < 31) {
+      //soberpeso
+      this.lectura = 'SOBREPESO';
+      this.foto = ImcComponent.FOTO_SOBREPESO;
+    } else if (this.numerico >= 31) {
+      //obeso
+      this.lectura = 'OBESO'; //accedo al Enumerado como String
+      this.foto = ImcComponent.FOTO_OBESO;
+    }
+  }
 }
