dccadd48aff1117af9d84a4929026da8814ec783
Val <valexx55@gmail.com>
2026-04-22 14:02:42 +0200

    formulario template 1/2

--- src/app/components/formulario-alumno/formulario-alumno.component.ts
+++ src/app/components/formulario-alumno/formulario-alumno.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Alumno } from '../../models/alumno';
 
 @Component({
   selector: 'app-formulario-alumno',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario-alumno.component.html',
   styleUrl: './formulario-alumno.component.css'
 })
 export class FormularioAlumnoComponent {
+
+  alumno!:Alumno;
+
+
+  constructor ()
+  {
+    this.alumno = new Alumno()
+  }
+  crearAlumno()
+  {
+    console.log(`tocó crear Alumno`);
+  }
 
 }
--- src/app/models/alumno.ts
+++ src/app/models/alumno.ts
@@ -1,22 +1,55 @@
-export interface Alumno {
-  
-   id: number,
-   nombre: string,
-   apellido: string,
-   email: string,
-   edad: number,
-   creadoEn:string
+export class Alumno {
 
-     //imporatante que coincidan los nombres de los atributos del JSON
-    /*
+    id:number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    edad: number;
+    creadoEn: string;
+
+    constructor()
     {
-    "id": 12,
-    "nombre": "Vinicius Pele",
-    "apellido": "Jr O Rei",
-    "email": "vini@correo.es",
-    "edad": 20,
-    "creadoEn": "2022-02-15T20:46:45.928+00:00",
-    "fotoHashCode": null
-  }
-    */
+        this.id=0;
+        this.nombre= '';
+        this.apellido= '';
+        this.email = ''; 
+        this.edad = 0;
+        this.creadoEn = ''; 
+    }
+    
+
+    /** {
+        "id": 12,
+        "nombre": "Vinicius Pele",
+        "apellido": "Jr O Rei",
+        "email": "vini@correo.es",
+        "edad": 20,
+        "creadoEn": "2022-02-15T20:46:45.928+00:00",
+        "fotoHashCode": null
+    } */
 }
+
+
+
+// export interface Alumno {
+  
+//    id: number,
+//    nombre: string,
+//    apellido: string,
+//    email: string,
+//    edad: number,
+//    creadoEn:string
+
+//      //imporatante que coincidan los nombres de los atributos del JSON
+//     /*
+//     {
+//     "id": 12,
+//     "nombre": "Vinicius Pele",
+//     "apellido": "Jr O Rei",
+//     "email": "vini@correo.es",
+//     "edad": 20,
+//     "creadoEn": "2022-02-15T20:46:45.928+00:00",
+//     "fotoHashCode": null
+//   }
+//     */
+// }
