511dd33ce7515f26bf250468817a36a7954e7cc7
Val <valexx55@gmail.com>
2026-04-24 11:07:10 +0200

    solucion leyendo persona y coche en paso 1 y 2

--- src/app/components/contratacion/paso1/paso1.component.ts
+++ src/app/components/contratacion/paso1/paso1.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DatosPersona } from '../contratacion.state';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { guardarPersona } from '../contratacion.actions';
 import { FormsModule } from '@angular/forms';
+import { AppState } from '../app.state';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-paso1',
@@ -11,15 +13,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './paso1.component.html',
   styleUrl: './paso1.component.css'
 })
-export class Paso1Component {
+export class Paso1Component implements OnInit {
 
   persona:DatosPersona = {nombre: '', edad: null, sexo: ''};
   
   //Router: para navegar programáticamente
   //Store para acceder a Redux
-  constructor(private router:Router, private store:Store)
+  constructor(private router:Router, private store: Store<AppState>)
   {
-
+      
+  }
+  
+  async ngOnInit(): Promise<void> {
+      let datosPersonaGuardados = await firstValueFrom  (this.store.select(estado=>estado.contratacion.persona));
+      this.persona = {...datosPersonaGuardados}
   }
 
   siguiente()
--- src/app/components/contratacion/paso2/paso2.component.ts
+++ src/app/components/contratacion/paso2/paso2.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { DatosCoche } from '../contratacion.state';
 import { guardarCoche } from '../contratacion.actions';
 import { FormsModule } from '@angular/forms';
+import { AppState } from '../app.state';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-paso2',
@@ -11,20 +13,30 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './paso2.component.html',
   styleUrl: './paso2.component.css'
 })
-export class Paso2Component {
+export class Paso2Component implements OnInit {
 
 
   coche: DatosCoche = { matricula: '', modelo: '' };
    //Router: para navegar programáticamente
   //Store para acceder a Redux
-  constructor(private router:Router, private store:Store)
+   constructor(private router:Router, private store: Store<AppState>)
   {
-
+    
   }
+  
+  
+  async ngOnInit() {
+
+    let datoCocheGuardado = await firstValueFrom( this.store.select(estado=> estado.contratacion.coche))
+    this.coche = {...datoCocheGuardado} //Importante una nueva referencia, si no, no deja leer y actualizar con ngModel después
+  }
+
+  
 
    anterior() {
     this.router.navigateByUrl('/paso-1');
   }
+
 
   siguiente() {
     this.store.dispatch(guardarCoche({ coche: this.coche }));
